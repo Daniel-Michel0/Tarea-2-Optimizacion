f4d4219d2d65b3c9e8bbecf861ddac5d0a726bb1
Daniel Michel <timydan@gmail.com>
2023-11-12 01:38:42 -0300

    Changes to GG

--- TSP-GG.py
+++ TSP-GG.py
@@ -2,19 +2,20 @@ import pulp
 import sys
 from utilidad import read_atsp
 
+def agregar_corte_gg(problema, solucion_no_entera):
+    # Agrega un corte GG al problema
+    problema += pulp.lpSum(x[(i, j)] for (i, j) in solucion_no_entera) <= len(solucion_no_entera) - 1
+
 # Crear un problema de minimización
 problema = pulp.LpProblem("Problema_de_Rutas_Mínimas", pulp.LpMinimize)
 
-# Obtener el archivo como parametro en la terminal
+# Obtener el archivo como parámetro en la terminal
 if len(sys.argv) < 2:
     print("Por favor, ingresa el nombre del archivo .atsp")
     sys.exit(1)
 
 path = sys.argv[1]
 graph = read_atsp(path)
-# Imprimir la matriz
-for line in graph:
-    print(line)
 
 n = len(graph)
 
@@ -25,35 +26,44 @@ A = [(i, j) for i in V for j in V if i != j]
 # Variables binarias x_ij
 x = pulp.LpVariable.dicts("x", A, 0, 1, pulp.LpBinary)
 
-# Variables de orden u_i
-g = pulp.LpVariable.dicts("u", V, 0, n - 1, pulp.LpInteger)
+# Variables de flujo y_i
+y = pulp.LpVariable.dicts("y", V, 0, n - 1, pulp.LpInteger)
 
 # Función objetivo: Minimizar la distancia total
 problema += pulp.lpSum(graph[i][j] * x[(i, j)] for (i, j) in A)
 
 # Restricciones
 for i in V:
-    # Cada nodo debe ser visitado exactamente una vez
     problema += pulp.lpSum(x[(i, j)] for j in V if i != j) == 1
 
 for j in V:
-    # Cada nodo debe ser salido exactamente una vez
     problema += pulp.lpSum(x[(i, j)] for i in V if i != j) == 1
 
-# Restricción de subtour
-for i in V:
+for i in V[1:]:
     for j in V:
         if i != j:
-            if i != 0 and j != 0:
-                problema += g[i] - g[j] + (n - 1) * x[(i, j)] <= n - 2
+            problema += y[i] - y[j] + n * x[(i, j)] <= n - 1
 
+# Resolver el problema con GG
+while True:
+    # Resolver el problema
+    problema.solve()
 
-# Resolver el problema
-problema.solve()
+    # Imprimir la solución
+    print("Status:", pulp.LpStatus[problema.status])
+    for (i, j) in A:
+        if x[(i, j)].varValue == 1:
+            print(f"x({i},{j}) = 1")
+    print("Optimal value =", pulp.value(problema.objective))
 
-# Imprimir la solución
-print("Status:", pulp.LpStatus[problema.status])
-for (i, j) in A:
-    if x[(i, j)].varValue == 1:
-        print(f"x({i},{j}) = 1")
-print("Optimal value =", pulp.value(problema.objective))
+    # Verificar si la solución es entera
+    if all(var.varValue.is_integer() for var in problema.variables()):
+        print("Solución entera encontrada.")
+        break
+    else:
+        print("Añadiendo corte de GG.")
+        # Identificar una solución no entera
+        solucion_no_entera = [(i, j) for (i, j) in A if x[(i, j)].varValue % 1 != 0]
+
+        # Agregar corte de GG al problema
+        agregar_corte_gg(problema, solucion_no_entera)
